0275ebfb87d035d96fbfb16b6ec637124374a9dd
Vitória Oliveira <paula.vitoria.de.oliveira@outlook.com>
2026-05-20 09:03:35 -0300

    Refactor main.jsx to use HashRouter and update imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </StrictMode>
-);
+  </HashRouter>
+)
